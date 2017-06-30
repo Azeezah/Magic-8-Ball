14b6224165d04fd8cbaf8d55df48c158a250bf8f
Azeezah Muhammad <muhammad.azeezah@infotech.hamptonu.edu>
2017-06-30 14:58:58 -0400

    Move output to center of 8 ball

--- script.js
+++ script.js
@@ -4,21 +4,32 @@ $(document).ready(onLoad);
 
 function shakeBall(){
 	var ball = $(".ball");
-	ball.animate({"margin-top":"0px"}, 400);
-	ball.animate({"margin-top":"40px"}, 250);
+	var text = $('#whitepart')
+	text.addClass('predictionText');
+
+	ball.animate({"margin-top":"0px"}, 600, 
+		function(){
+			ball.animate({"margin-top":"40px"}, 250, 
+				function(){
+					text.removeClass('predictionText');
+					text.text('8');
+				}
+			);
+		}
+	);
 }
 
 function makePrediction(){
 	if ($('#question').val().indexOf('?') > -1){
-		$('#output').text(generatePrediction());
+		$('#whitepart').text(generatePrediction());
 	} else {
-		$('#output').text('Ask a question!');
+		$('#whitepart').text('Ask a question!');
 	}
 	shakeBall();
 }
 
 function generatePrediction(){
-	var predictions = ['yeah sure', 'no...', 'kinda' ,'probably not', 'um... maybe...', 'absolutely!', 'definitely not'];
+	var predictions = ['yep', 'I would say so', 'yeah sure', 'no...', 'kinda' ,'probably not', 'um... maybe...', 'absolutely!', 'definitely not'];
 	prediction = predictions[Math.floor(Math.random()*predictions.length)];
 	return prediction;
 }
